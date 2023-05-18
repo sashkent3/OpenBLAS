0835ae545c58453a343b11bff712bfdfa2e17705
Alexander Zinov <sashkent3@gmail.com>
2023-05-18 09:49:16 +0000

    add copyright notice

--- kernel/riscv64/sgemm_kernel_8x8_vfast.c
+++ kernel/riscv64/sgemm_kernel_8x8_vfast.c
@@ -1,22 +1,34 @@
-// #include <assert.h>
+/***************************************************************************
+Copyright (c) 2023, The OpenBLAS Project
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in
+the documentation and/or other materials provided with the
+distribution.
+3. Neither the name of the OpenBLAS project nor the names of
+its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE OPENBLAS PROJECT OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*****************************************************************************/
 
 #include "common.h"
 
 
 int CNAME(BLASLONG M, BLASLONG N, BLASLONG K, FLOAT alpha, FLOAT* A, FLOAT* B, FLOAT* C, BLASLONG ldc) {
-    // assume that SGEMM_UNROLL_M == VLMAX
-    // assert(__riscv_vsetvlmax_e32m1() == SGEMM_UNROLL_M);
-    // assert(SGEMM_UNROLL_M == 8);
-    // assert(SGEMM_UNROLL_N == 8);
-    // printf("SGEMM A:\n");
-    // for (size_t i = 0; i < M * K; ++i) {
-    //     printf("%g ", A[i]);
-    // }
-    // printf("\nSGEMM B:\n");
-    // for (size_t i = 0; i < K * N; ++i) {
-    //     printf("%g ", B[i]);
-    // }
-    // printf("\n");
     vfloat32m1_t A_v, C_col_0, C_col_1, C_col_2, C_col_3, C_col_4, C_col_5, C_col_6, C_col_7;
     FLOAT *A_ptr, *B_ptr, *C_ptr_l, *C_ptr_s;
     size_t vl;
